efa8b95877c900143bb65fa27153adcb54c60d95
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2024-02-22 17:03:50 +0900

    .

--- server.js
+++ server.js
@@ -1,44 +1,47 @@
-// 서버 측 (Node.js)
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server, { path: '/socket.io' });
-//const io = socketIO(server);
+const io = socketIO(server);
 
-// 모바일 클라이언트와 PC 클라이언트를 저장할 변수
 let mobileClient;
 let pcClient;
 
-app.get('/socket.io', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
-  });
-
+});
 
 io.on('connection', (socket) => {
-  console.log('Client connected:', socket.id);
+    console.log('Client connected:', socket.id);
 
-  socket.on('mobile', () => {
-    // 모바일 클라이언트로 설정
-    mobileClient = socket;
-  });
+    socket.on('mobile', () => {
+        mobileClient = socket;
+    });
 
-  socket.on('pc', () => {
-    // PC 클라이언트로 설정
-    pcClient = socket;
-  });
+    socket.on('pc', () => {
+        pcClient = socket;
+    });
 
-  socket.on('dataFromMobile', (data) => {
-    // 모바일 클라이언트로부터 데이터를 받아서 PC 클라이언트로 중계
-    if (pcClient) {
-      pcClient.emit('dataToPC', data);
-    }
-  });
+    socket.on('dataFromMobile', (data) => {
+        if (pcClient) {
+            pcClient.emit('dataToPC', data);
+        }
+    });
+
+    socket.on('dataFromPC', (data) => {
+        if (mobileClient) {
+            mobileClient.emit('dataToMobile', data);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id);
+    });
 });
 
 const PORT = process.env.PORT || 3567;
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Relay Server running on http://localhost:${PORT}`);
 });
