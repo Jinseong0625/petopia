09378cf749af43992ae59d30b64b81f76ad1fff1
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2024-02-26 16:16:27 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const corsOptions = {
