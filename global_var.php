819ca23a8bc31cb9deccef93c44f5a89a5ae8ac0
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2024-03-19 10:38:03 +0900

    Update global_var.php

--- global_var.php
+++ global_var.php
@@ -1,10 +1,16 @@
 <?php
 
-define("_HOST_", "13.125.36.197");  //Live
-#define("_HOST_", "34.64.193.173");  //Live
-define("_USER_", "Newto_1");
-define("_PWD_", "newto123");
-define("_DB_", "photoDB");
+define("_HOST_", "218.38.65.83");  //Live
+// define("_HOST_", "rds-an2-inside-dev-go-newto-maria.czyj3edktxic.ap-northeast-2.rds.amazonaws.com"); //test
+define("_USER_", "sysadmin1");
+define("_PWD_", "Newto123");
+define("_DB_", "MVGameDB");
 define("_PORT_", "3306");
+
+define("_LOGDBHOST_", "218.38.65.83");
+define("_LOGDBUSER_", "sysadmin1");
+define("_LOGDBPWD_", "Newto123");
+define("_LOGDBDB_", "MVLogDB");
+define("_LOGDBPORT_", "3306");
 
 ?>
