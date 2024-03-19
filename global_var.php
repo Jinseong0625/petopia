44ea75d34f397beeb24acd63c368a78c77261fbf
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2024-03-19 10:40:03 +0900

    Update global_var.php

--- global_var.php
+++ global_var.php
@@ -4,7 +4,7 @@ define("_HOST_", "218.38.65.83");  //Live
 // define("_HOST_", "rds-an2-inside-dev-go-newto-maria.czyj3edktxic.ap-northeast-2.rds.amazonaws.com"); //test
 define("_USER_", "sysadmin1");
 define("_PWD_", "Newto123");
-define("_DB_", "MVGameDB");
+define("_DB_", "PetopiaGameDb");
 define("_PORT_", "3306");
 
 define("_LOGDBHOST_", "218.38.65.83");
