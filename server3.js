6b7e7528530d26586aef5edbb54da13df6d47d00
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2024-03-21 15:52:16 +0900

    Update server3.js

--- server3.js
+++ server3.js
@@ -2,45 +2,40 @@ const WebSocket = require('ws');
 const { eSocketPacket } = require('../petopia/common/socketpacket.js');
 const { eSendTarget } = require('../petopia/common/socketTarget.js');
 
-let channels = {}; // 여러 채널을 저장하는 객체
+let channels = {}; // 채널을 저장하는 객체
 let channelCounter = 1; // 채널 번호를 증가시키기 위한 카운터
 
-const wss = new WebSocket.Server({ port: 3567 });
-
+const wss = new WebSocket.Server({ port: 3567 }); // WebSocket 서버 생성
 
 console.log('Server listening on port 3567');
 
+// 클라이언트 연결 시
 wss.on('connection', (ws) => {
     console.log('Client connected');
 
+    // 클라이언트로부터 메시지 수신 시
     ws.on('message', (data) => {
         try {
             const clientMessage = JSON.parse(data);
     
-            // 여러 채널을 다루기 위해, 메시지의 채널 정보를 확인
+            // 메시지의 채널 정보, 패킷, 타겟을 확인
             const channel = clientMessage.channel;
             const packet = clientMessage.packet;
             const target = clientMessage.target;
     
             switch (packet) {
-                case eSocketPacket.create_channel: // packet = 1 이건 채널을 생성하려고 하는것이고 
-                    handlePacketZero(ws,packet);
+                case eSocketPacket.create_channel: // 채널 생성
+                    handlePacketZero(ws, packet);
                     break;
-    
-                case eSocketPacket.join_channel: // packet = 2 이건 생성되어 있는 채널에 클라를 추가하는 것이고
+                case eSocketPacket.join_channel: // 채널 참여
                     handlePacketOne(channel, ws, data, packet);
                     break;
-                
-                case eSocketPacket.join_world: //packet = 3 이건 world로 클라를 보내는 역할을 하는것이고
+                case eSocketPacket.join_world: // world로 클라 전송
                     handlePackettwo(channel, ws, data, packet);
                     break;
-                
-                case eSocketPacket.exit_world: // packet = 4 이건 월드를 나가는 역할이다.
-                    handlePacketthree
+                case eSocketPacket.exit_world: // 월드 나가기
+                    handlePacketthree(channel, ws, data, packet);
                     break;
-
-                // 다른 패킷 추가 여기서 내가 볼땐  
-    
                 default:
                     handleDefaultPacket(channel, ws, data);
             }
@@ -48,45 +43,6 @@ wss.on('connection', (ws) => {
             console.error('Error handling data:', error);
         }
     });
-
-    // 클라이언트로부터 메시지 수신
-    /*ws.on('message', (data) => {
-        try {
-            const clientMessage = JSON.parse(data);
-
-            // 여러 채널을 다루기 위해, 메시지의 채널 정보를 확인
-            const channel = clientMessage.channel;
-            const packet = clientMessage.packet;
-
-            if (packet === 1) {
-                // 클라이언트가 packet 1을 보내면 새로운 채널을 생성하고 마스터 클라이언트로 설정
-                const newChannel = createChannel(ws);
-                ws.send(JSON.stringify({
-                    channelCreated: newChannel,
-                    packet: packet,
-                    message: newChannel
-                }));
-                console.log(`Channel ${newChannel} created. Master client: ${ws._socket.remoteAddress}`); // 수정된 부분
-            } else {
-                // 채널이 이미 존재하는 경우 클라이언트를 해당 채널에 추가
-                if (channels[channel]) {
-                    addClientToChannel(channel, ws);
-                    if (channels[channel].size > 1) {
-                        relayDataToClients(channel, ws, data);
-                    }
-                    console.log(`Client added to channel ${channel}: ${ws._socket.remoteAddress}`);
-                } else {
-                    console.error(`Channel ${channel} does not exist.`);
-                }
-            }
-
-
-            relayDataToClients(channel, ws, data);
-        } catch (error) {
-            console.error('Error handling data:', error);
-        }
-    });*/
-
 
     // 클라이언트 연결 해제 시
     ws.on('close', () => {
@@ -96,15 +52,17 @@ wss.on('connection', (ws) => {
     });
 });
 
+// 채널 생성 함수
 function createChannel(masterClient) {
     const newChannel = channelCounter++;
-    channels[newChannel] = new Set(); // 채널 생성 및 채널에 클라이언트 추가
+    channels[newChannel] = new Set(); // 채널 생성 및 클라이언트 추가
     channels[newChannel].add(masterClient);
-    console.log(`Channel ${newChannel} created. Master client: ${masterClient._socket.remoteAddress}`); // 수정된 부분
+    console.log(`Channel ${newChannel} created. Master client: ${masterClient._socket.remoteAddress}`);
     logChannelInfo(newChannel);
     return newChannel;
 }
 
+// 채널에 클라이언트 추가 함수
 function addClientToChannel(channel, client) {
     if (channels[channel]) {
         channels[channel].add(client);
@@ -114,6 +72,7 @@ function addClientToChannel(channel, client) {
     }
 }
 
+// 패킷 핸들러 - 채널 생성
 function handlePacketZero(ws, packet) {
     if (packet === eSocketPacket.create_channel) {
         const newChannel = createChannel(ws);
@@ -128,9 +87,10 @@ function handlePacketZero(ws, packet) {
     }
 }
 
+// 패킷 핸들러 - 채널 참여
 function handlePacketOne(channel, ws, data, packet) {
     if (packet === eSocketPacket.join_channel) {
-        // 클라이언트가 패킷 1 패킷을 보내면 해당 채널에 클라이언트를 추가
+        // 채널에 클라이언트 추가
         if (channels[channel]) {
             addClientToChannel(channel, ws);
             if (channels[channel].size > 1) {
@@ -147,9 +107,10 @@ function handlePacketOne(channel, ws, data, packet) {
     }
 }
 
+// 패킷 핸들러 - world로 클라 전송
 function handlePackettwo(channel, ws, data, packet) {
     if (packet === eSocketPacket.join_world) {
-        // 클라이언트가 패킷 2 패킷을 보내면 해당 채널에 클라이언트를 추가
+        // 채널에 클라이언트 추가
         if (channels[channel]) {
             addClientToChannel(channel, ws);
             if (channels[channel].size > 1) {
@@ -164,11 +125,12 @@ function handlePackettwo(channel, ws, data, packet) {
     }
 }
 
+// 패킷 핸들러 - 월드 나가기
 function handlePacketthree(channel, ws, data, packet) {
     if (packet === eSocketPacket.exit_world) {
-        // 클라이언트가 패킷 3 패킷을 보내면 해당 채널에서 클라이언트를 제거
+        // 채널에서 클라이언트 제거
         removeClient(ws);
-        // 채널에 변경 사항이 있을 경우 모든 클라이언트에게 새로운 채널 정보를 전달
+        // 채널에 변경 사항이 있을 경우 모든 클라이언트에게 새로운 채널 정보 전달
         relayDataToClients(channel, ws, data); // 모든 클라이언트에게 메시지 전송
         console.log(`Client exited from channel ${channel}: ${ws._socket.remoteAddress}`);
     } else {
@@ -176,6 +138,7 @@ function handlePacketthree(channel, ws, data, packet) {
     }
 }
 
+// 기본 패킷 핸들러
 function handleDefaultPacket(channel, ws, data) {
     try {
         const clientMessage = JSON.parse(data);
@@ -183,19 +146,19 @@ function handleDefaultPacket(channel, ws, data) {
         const packet = clientMessage.packet;
 
         switch (target) {
-            case eSendTarget.server: // 서버에 데이터를 전송할 경우
+            case eSendTarget.server: // 서버에 데이터 전송
                 relayDataToServer(ws, data);
                 break;
-            case eSendTarget.all: // 모든 클라이언트에 데이터를 전송할 경우
+            case eSendTarget.all: // 모든 클라이언트에 데이터 전송
                 relayDataToAllClients(channel, ws, data);
                 break;
-            case eSendTarget.master: // 마스터 클라이언트에 데이터를 전송할 경우
+            case eSendTarget.master: // 마스터 클라이언트에 데이터 전송
                 relayDataToMasterClient(channel, ws, data);
                 break;
-            case eSendTarget.anothers: // 다른 클라이언트에 데이터를 전송할 경우
+            case eSendTarget.anothers: // 다른 클라이언트에 데이터 전송
                 relayDataToOtherClients(channel, ws, data);
                 break;
-            case eSendTarget.target: // 특정 클라이언트에 데이터를 전송할 경우
+            case eSendTarget.target: // 특정 클라이언트에 데이터 전송
                 relayDataToTargetClient(channel, ws, data);
                 break;
             default:
@@ -206,12 +169,11 @@ function handleDefaultPacket(channel, ws, data) {
     }
 }
 
-
+// 클라이언트에게 데이터 전송 - 모든 클라이언트
 function relayDataToClients(channel, senderClient, data) {
     if (channels[channel]) {
         channels[channel].forEach((client) => {
             if (client !== senderClient && client.readyState === WebSocket.OPEN) {
-                //client.send(JSON.stringify(JSON.parse(data))); // 클라이언트에서 받은 데이터를 JSON으로 파싱하여 브로드캐스팅
                 client.send(JSON.stringify({
                     channelJoined: channel,
                     message: channel
@@ -223,11 +185,13 @@ function relayDataToClients(channel, senderClient, data) {
     console.log(`Data relayed to all clients on channel ${channel}:`, JSON.stringify(JSON.parse(data)));
 }
 
+// 클라이언트에게 데이터 전송 - 서버
 function relayDataToServer(ws, data) {
     console.log('Data relayed to server:', data);
     ws.send(data);
 }
 
+// 클라이언트에게 데이터 전송 - 모든 클라이언트
 function relayDataToAllClients(channel, senderClient, data) {
     console.log('Data relayed to all clients in channel', channel, ':', data);
     channels[channel].forEach(client => {
@@ -237,6 +201,7 @@ function relayDataToAllClients(channel, senderClient, data) {
     });
 }
 
+// 클라이언트에게 데이터 전송 - 마스터 클라이언트
 function relayDataToMasterClient(channel, senderClient, data) {
     console.log('Data relayed to master client in channel', channel, ':', data);
     const masterClient = Array.from(channels[channel])[0];
@@ -247,6 +212,7 @@ function relayDataToMasterClient(channel, senderClient, data) {
     }
 }
 
+// 클라이언트에게 데이터 전송 - 다른 클라이언트
 function relayDataToOtherClients(channel, senderClient, data) {
     console.log('Data relayed to other clients in channel', channel, ':', data);
     channels[channel].forEach(client => {
@@ -256,6 +222,7 @@ function relayDataToOtherClients(channel, senderClient, data) {
     });
 }
 
+// 클라이언트에게 데이터 전송 - 특정 클라이언트
 function relayDataToTargetClient(channel, senderClient, data) {
     console.log('Data relayed to target client in channel', channel, ':', data);
     try {
@@ -272,9 +239,9 @@ function relayDataToTargetClient(channel, senderClient, data) {
     }
 }
 
-
+// 클라이언트 제거 함수
 function removeClient(client) {
-    // 클라이언트가 연결 해제될 때 해당 클라이언트를 모든 채널에서 제거
+    // 클라이언트 연결 해제 시 해당 클라이언트를 모든 채널에서 제거
     Object.keys(channels).forEach((channel) => {
         channels[channel].delete(client);
         // 채널이 비어있다면 삭제
@@ -289,14 +256,17 @@ function removeClient(client) {
     console.log('Client removed');
 }
 
+// 채널 정보 로깅 함수
 function logChannelInfo(channel) {
     console.log(`Channel ${channel} has ${channels[channel].size} client(s).`);
 }
+
+// 연결된 클라이언트 로깅 함수
 function logConnectedClients() {
     console.log('Connected clients:', wss.clients.size);
 }
 
-// 서버가 시작될 때마다 channels 객체 초기화
+// 서버 시작 시 채널 초기화 함수
 function resetChannels() {
     channels = {};
     channelCounter = 1; // 채널 번호 초기화
