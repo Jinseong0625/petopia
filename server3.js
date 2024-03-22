c3df4cbfaf8239abe7b4b9cdeda50b13ca398a68
Jinseong0625 <123522700+Jinseong0625@users.noreply.github.com>
2024-03-22 12:21:37 +0900

    Update server3.js

--- server3.js
+++ server3.js
@@ -147,7 +147,7 @@ function relayDataToAllClients(channel, data) {
         console.log('Data relayed to all clients in channel', channel, ':', JSON.stringify(JSON.parse(data)));
         channels[channel].forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
-                client.send(JSON.stringify(data));
+                client.send(data);
             } else {
                 console.error('Client connection is not open, message not sent.');
             }
